7a7af04c8180414653dc7424ab683a6fe28f94d7
Iurii <kenu21@gmail.com>
2019-10-16 17:00:44 +0300

    added hw-30-1

---
+++ src/main/java/mate/academy/spring/entity/Book.java
@@ -0,0 +1,66 @@
+package mate.academy.spring.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "price")
+    private double price;
+
+    public Book() {}
+
+    public Book(String name, int year, double price) {
+        this.name = name;
+        this.year = year;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
